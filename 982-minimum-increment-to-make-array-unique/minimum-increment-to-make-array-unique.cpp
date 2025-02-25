142548423f1e6ce503b0b58731d53157280b4fc4
kashishpratap9 <kashishpratap4@gmail.com>
2025-02-25 15:43:39 +0530

    Time: 1306 ms (5.07%) | Memory: 113.9 MB (7.23%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
+        sort( nums.begin(), nums.end());
+        vector<int>ans(nums.size());
+        unordered_set<int>st;
+        ans[0]=nums[0];
+        st.insert(ans[0]);
+        int moves=0;
+         for( int i=1;i<nums.size();i++){
+            if(ans[i-1]==nums[i]||st.find(nums[i])!=st.end()){
+                ans[i]= ans[i-1]+1;
+                st.insert(ans[i]);
+                moves+=ans[i]-nums[i];
+            }else{
+                ans[i]=nums[i];
+                st.insert(ans[i]);
+            }
+         }
 
-       sort(nums.begin(), nums.end());
-       int moves=0;
-        for( int i=1;i<nums.size();i++){
-             if( nums[i-1]==nums[i]){
-                nums[i]= nums[i]+1;
-                moves++;
-             }else if( nums[i-1]> nums[i]){
-                int val= nums[i];
-                nums[i]= nums[i-1]+1;
-                moves+=(nums[i]-val);
-             }
-        }
-
-        return moves;
-
+         for( int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
+         }
+         return moves;
         
     }
 };
