9e0b3a7903d595e33f4b453ddbf4522a9965b549
kashishpratap9 <kashishpratap4@gmail.com>
2025-03-04 11:01:27 +0530

    Time: 39 ms (7.98%) | Memory: 7.8 MB (67.84%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-bool powerofthree(int i, int n, unordered_set<int>&ans, vector<int>&power_set){
+bool powerofthree(int power, int n){
     if(n==0){
         return true;
     }
 
-    if(i>=power_set.size() || n<0){
+    if(pow(3, power) > n){
          return false;
     }
-    bool inc=false;
-    if(power_set[i]<=n && ans.find(power_set[i])==ans.end()){
-        ans.insert(power_set[i]);
-        inc=powerofthree(i, n-power_set[i], ans, power_set);
-        ans.erase(power_set[i]);
-    }
+    // bool inc=false;
+    // if(power_set[i]<=n && ans.find(power_set[i])==ans.end()){
+    //     ans.insert(power_set[i]);
+    bool inc=powerofthree(power+1, n-pow(3, power));
+    //     ans.erase(power_set[i]);
+    // }
 
-     bool exc=powerofthree(i+1, n,ans, power_set);
+     bool exc=powerofthree(power+1, n);
      return inc||exc;
 
 }
     bool checkPowersOfThree(int n) {
-    vector<int>power_set={1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323};
-    if( n==6378022|| n==6574365){
-        return true;
-    }
-    unordered_set<int>ans;
+    // vector<int>power_set={1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323};
+    // if( n==6378022|| n==6574365){
+    //     return true;
+    // }
+    // unordered_set<int>ans;
     // vector<vector<int>>result;
-    return powerofthree(0, n,ans, power_set);
+    return powerofthree(0, n);
    
 
         
