932af3ca58e1246e8d29e9aa64947816729d1f81
kashishpratap9 <kashishpratap4@gmail.com>
2025-03-17 13:15:44 +0530

    Time: 77 ms (42.92%) | Memory: 133.6 MB (54.72%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        unordered_map<int, vector<int>>adj;
-        unordered_map<int, int>indegree;
+        // unordered_map<int, vector<int>>adj;
+        vector<int>indegree(n, 0);
         for(auto e: roads){
             int u=e[0];
             int v=e[1];
@@ -10,26 +10,36 @@ public:
             indegree[v]++;
         }
 
-        auto lambda= [](pair<int, int>&p1 , pair<int, int>&p2){
-            return p1.second>p2.second;
-        };
+        sort(indegree.begin(), indegree.end());
+        int cn=1;
+        long long sum=0;
+        for( int i=0;i<indegree.size();i++){
+            if(cn<=n){
+            long long val = (long long)indegree[i] * cn;
+            sum+=val;
+            cn++;
+            }
 
-        vector<pair<int, int>>vec(indegree.begin(), indegree.end());
-        sort(vec.begin(), vec.end(), lambda);
-        unordered_map<int, int>mp1;
-       int cn=n;
-      for(int i=0;i<vec.size();i++){
-         mp1[vec[i].first]=cn;
-         cn--;
-      }
-      long long sum=0;
-      for( auto e:roads){
-        int u=e[0];
-        int v=e[1];
-        sum+=mp1[u]+mp1[v];
-      }
-    
+        }
 
+    //     auto lambda= [](pair<int, int>&p1 , pair<int, int>&p2){
+    //         return p1.second>p2.second;
+    //     };
+
+    //     vector<pair<int, int>>vec(indegree.begin(), indegree.end());
+    //     sort(vec.begin(), vec.end(), lambda);
+    //     unordered_map<int, int>mp1;
+    //    int cn=n;
+    //   for(int i=0;i<vec.size();i++){
+    //      mp1[vec[i].first]=cn;
+    //      cn--;
+    //   }
+    //   long long sum=0;
+    //   for( auto e:roads){
+    //     int u=e[0];
+    //     int v=e[1];
+    //     sum+=mp1[u]+mp1[v];
+    //   }
 
         return sum;
 
