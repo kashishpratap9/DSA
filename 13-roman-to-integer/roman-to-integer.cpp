a9d3eb6171c1be465e5e261eb85fc4933ac3f7ec
kashishpratap9 <kashishpratap4@gmail.com>
2025-04-18 00:01:07 +0530

    Time: 3 ms (73.30%) | Memory: 12.3 MB (64.63%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,43 +1,50 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<string , int>mp;
-        mp["I"]=1;
-        mp["IV"]=4;
-        mp["V"]=5;
-        mp["IX"]=9;
-        mp["X"]=10;
-        mp["XL"]=40;
-        mp["L"]=50;
-        mp["XC"]=90;
-        mp["C"]=100;
-         mp["CD"]=400;
-        mp["D"]=500;
-        mp["CM"]=900;
-        mp["M"]=1000;
+        unordered_map<char , int>mp;
+        mp['I']=1;
+       
+        mp['V']=5;
+   
+        mp['X']=10;
+      
+        mp['L']=50;
+   
+        mp['C']=100;
+ 
+        mp['D']=500;
+  
+        mp['M']=1000;
         int result=0;
          for( int i=0;i<s.size();i++){
-            string v="";
-            if( i+1 <s.size() && s[i]=='I' && (s[i+1]=='V'|| s[i+1]=='X')){
-                v.push_back(s[i]);
-                v.push_back(s[i+1]);
-                result+=mp[v];
+            if( i+1 <s.size() && s[i]=='I' && s[i+1]=='V'){
+                    result+=4;
                 i++;
-            }else if( i+1 <s.size() && s[i]=='X' && (s[i+1]=='L'|| s[i+1]=='C')){
-                 v.push_back(s[i]);
-                v.push_back(s[i+1]);
-                result+=mp[v];
-                i++;
-
-            }else if(i+1 <s.size() && s[i]=='C' && (s[i+1]=='M'|| s[i+1]=='D')){
-                 v.push_back(s[i]);
-                v.push_back(s[i+1]);
-                result+=mp[v];
+            }else if(i+1 <s.size() && s[i]=='I' && s[i+1]=='X'){
+                result+=9;
+                 i++;
+            }
+            else if( i+1 <s.size() && s[i]=='X' && s[i+1]=='L'){
+                    result+=40;
                 i++;
 
-            }else{
-                v+=s[i];
-             result+=mp[v];
+            }else if(i+1 <s.size() && s[i]=='X' && s[i+1]=='C'){
+                 result+=90;
+                i++;
+
+            }
+            else if(i+1 <s.size() && s[i]=='C' && s[i+1]=='D'){
+                    result+=400;
+                i++;
+
+            } else if(i+1 <s.size() && s[i]=='C' && s[i+1]=='M'){
+                    result+=900;
+                i++;
+
+            }
+            else{
+            
+             result+=mp[s[i]];
             }
 
             
