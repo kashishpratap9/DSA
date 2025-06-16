97f5e0af9b990f9630c859de9e832c5426ce3843
kashishpratap9 <kashishpratap4@gmail.com>
2025-06-16 10:47:28 +0530

    Time: 902 ms (13.64%) | Memory: 11.6 MB (36.37%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,62 +1,43 @@
 class Solution {
 public:
-vector<vector<int>>directions={{1,0},{-1, 0},{0,1},{0,-1}};
-bool dfs( int i, int j , int k, string ans,string&word, vector<vector<char>>& board){
-    int n =board.size();
-    int m =board[0].size();
-    if(ans.size()==word.size()){
+
+
+bool wordSearch( int i, int j,int k ,vector<vector<char>>&board , string word){
+    if(k==word.size()){
         return true;
     }
-
-    if( i<0 || j<0 || i>=n || j>=m || board[i][j]!=word[k] || board[i][j]=='$'){
+    //out of bound
+    if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]=='$'){
         return false;
     }
-
+    
+   if( board[i][j]==word[k]){
     char val = board[i][j];
-    ans+=val;
     board[i][j]='$';
-    k++;
-    // for(auto dir: directions){
-    //     int i_=i+ dir[0];
-    //     int j_= j+ dir[1];
-    //     if( i_>=0 && i_<n && j_>=0&&  j_<m){
-    //         dfs( i_, j_,k+=1,flag,word, board);
-    //         board[i][j]=val;
-    //     }
-    // }
-    bool d=dfs(i+1, j, k,ans, word, board);
-    bool u=dfs(i-1, j, k,ans,word, board);
-    bool l=dfs(i, j-1, k,ans, word, board);
-    bool r=dfs(i, j+1, k,ans,word, board);
-    board[i][j]=val;
-    ans.pop_back();
+     bool found =wordSearch( i+1, j, k+1, board, word) || wordSearch( i-1, j, k+1, board, word) || wordSearch( i, j+1, k+1, board, word) || wordSearch( i, j-1, k+1, board, word);
+        board[i][j]=val;
 
+        return found;
 
-    return d||u||l||r;
+   }
 
+   return false;
    
-
-
-
 
 }
     bool exist(vector<vector<char>>& board, string word) {
-        int n =board.size();
-         int m =board[0].size();
-         int k=0;
-          for( int i=0;i<n;i++){
-            for( int j=0;j<m;j++){
-                string ans="";
+        int k=0;
+         bool ans;
+        for( int i=0;i<board.size();i++){
+            for( int j=0;j<board[0].size();j++){
                 if(board[i][j]==word[0]){
-                    if(dfs(i, j,0,ans, word, board)){
-                        return true;
-                    }
-                    
+                   if(wordSearch(i, j, k, board, word)){
+                    return true;
+                   }
                 }
-
             }
-          }
-          return false;
+        }
+        return false;
         
     }
 };
